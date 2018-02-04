a9cb3ccfca16575776ed8beeb42435031d4c28bd
johnSjo <john.sjostrom@casumo.com>
2018-02-04 13:16:10 +0100

    creating the board collection

--- server/main.js
+++ server/main.js
@@ -1,5 +1,18 @@
 import { Meteor } from 'meteor/meteor';
+import Games from '../imports/api/collections/games.js'; // import Games collection
+
+const SMALL_BOARD = 15;
+const BIG_BOARD = 19;
 
 Meteor.startup(() => {
-  // code to run on server at startup
+
+    // remove all existing game documents
+    Games.remove({});
+
+    const size = SMALL_BOARD;
+    const board = Array(size).fill(Array(size).fill(null));
+
+    const gameDoc = { board };
+
+    Games.insert(gameDoc);
 });
